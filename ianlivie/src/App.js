d0488ac48593966ae1b9a7d3b2f341fa1d859d39
Luke Welch <lukewelch@Lukes-MacBook-Pro-2.local>
2020-04-11 16:25:52 +0100

    Starter Files

--- ianlivie/src/App.js
+++ ianlivie/src/App.js
@@ -3,12 +3,15 @@ import { Container } from "semantic-ui-react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import AboutPage from "./components/AboutPage/AboutPage";
+import ContactPage from "./components/ContactPage/ContactPage"
 
 const App = () => {
   return (
     <Fragment>
       <Route exact path={"/"} component={HomePage} />
       <Route exact path={"/about"} component={AboutPage} />
+      <Route exact path={"/contact"} component={ContactPage} />
+  
     </Fragment>
   );
 };
---
+++ ianlivie/src/components/ContactPage/ContactPage.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+
+class ContactPage extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className={'div'}>GET WEBSOTRM</div>
+        )
+    }
+}
+
+export default ContactPage
