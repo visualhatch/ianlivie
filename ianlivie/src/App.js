1b981a382067d0a152a3f025d8db65d48139f726
Dave Evans <davelx@icloud.com>
2020-04-11 10:13:43 -0500

    Starter Files

--- ianlivie/src/App.js
+++ ianlivie/src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import { Route, Switch, withRouter } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import AboutPage from "./components/AboutPage/AboutPage";
 
 const App = () => {
   return (
     <Fragment>
-      <Route exact path={"/"} component={HomePage2} />
+      <Route exact path={"/"} component={HomePage} />
+      <Route exact path={"/about"} component={AboutPage} />
     </Fragment>
   );
 };
